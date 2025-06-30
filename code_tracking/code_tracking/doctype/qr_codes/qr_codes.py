13c15cbad81d87e69c1b5bce7a1826c6b177bbad
wanguimbutu <wanguimbutu@gmail.com>
2025-06-30 14:20:30 +0300

    correct fetching

--- code_tracking/code_tracking/doctype/qr_codes/qr_codes.py
+++ code_tracking/code_tracking/doctype/qr_codes/qr_codes.py
@@ -9,7 +9,7 @@ from frappe import _
 class QrCodes(Document):
 	pass
 
+
 @frappe.whitelist()
 def check_qr_exists(qr_code):
-    qr = frappe.db.exists("QR Codes", {"qr_code": qr_code})
-    return bool(qr)
+    return bool(frappe.db.exists("QR Codes", {"qr_code": qr_code}))
