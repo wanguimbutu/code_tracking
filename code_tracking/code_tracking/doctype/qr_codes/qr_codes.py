79851efe0a5e5120c81418ebc54c29b0b71374d2
wanguimbutu <wanguimbutu@gmail.com>
2025-07-08 11:29:48 +0300

    add authenticator

--- code_tracking/code_tracking/doctype/qr_codes/qr_codes.py
+++ code_tracking/code_tracking/doctype/qr_codes/qr_codes.py
@@ -13,3 +13,7 @@ class QrCodes(Document):
 @frappe.whitelist()
 def check_qr_exists(qr_code):
     return bool(frappe.db.exists("Qr Codes", {"qr_code": qr_code}))
+
+@frappe.whitelist()
+def validate_authenticator(qrcode):
+    return bool(frappe.db.exists("Authenticator", {"qrcode": qrcode}))
