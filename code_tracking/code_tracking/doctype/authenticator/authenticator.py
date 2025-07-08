3e77f7f7e8998041c3108358fecae470458162b6
wanguimbutu <wanguimbutu@gmail.com>
2025-07-08 11:35:10 +0300

    add validation function

--- code_tracking/code_tracking/doctype/authenticator/authenticator.py
+++ code_tracking/code_tracking/doctype/authenticator/authenticator.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2025, wanguimbutu@gmail.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Authenticator(Document):
 	pass
+
+@frappe.whitelist()
+def validate_authenticator(qrcode):
+    return bool(frappe.db.exists("Authenticator", {"qrcode": qrcode}))
--- code_tracking/code_tracking/doctype/qr_codes/qr_codes.py
+++ code_tracking/code_tracking/doctype/qr_codes/qr_codes.py
@@ -14,6 +14,3 @@ class QrCodes(Document):
 def check_qr_exists(qr_code):
     return bool(frappe.db.exists("Qr Codes", {"qr_code": qr_code}))
 
-@frappe.whitelist()
-def validate_authenticator(qrcode):
-    return bool(frappe.db.exists("Authenticator", {"qrcode": qrcode}))
